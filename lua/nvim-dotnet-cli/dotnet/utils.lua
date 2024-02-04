876df257a7dcc765d7668bfa0ea3d400b59285b6
Christoffer Berglund <christoffer.berglund@icloud.com>
2024-02-04 12:03:27 +0100

    Refactor find method and class names

--- lua/nvim-dotnet-cli/dotnet/utils.lua
+++ lua/nvim-dotnet-cli/dotnet/utils.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.find_current_class_name()
+local function find_name_with_query(query_string, target_capture_name)
 	local parser = vim.treesitter.get_parser(0, "c_sharp")
 	local tree = parser:parse()[1]
 	local root = tree:root()
@@ -8,20 +8,13 @@ function M.find_current_class_name()
 	local current_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 	current_line = current_line - 1
 
-	local query = vim.treesitter.query.parse(
-		"c_sharp",
-		[[
-        (class_declaration
-            name: (identifier) @classname
-        )
-    ]]
-	)
+	local query = vim.treesitter.query.parse("c_sharp", query_string)
 
 	local start_row, _, end_row, _ = root:range()
 
 	for id, node in query:iter_captures(root, 0, start_row, end_row) do
 		local name = query.captures[id]
-		if name == "classname" then
+		if name == target_capture_name then
 			if start_row <= current_line and current_line <= end_row then
 				return vim.treesitter.get_node_text(node, 0)
 			end
@@ -31,35 +24,26 @@ function M.find_current_class_name()
 	return nil
 end
 
+function M.find_current_class_name()
+	return find_name_with_query(
+		[[
+        (class_declaration
+            name: (identifier) @classname
+        )
+    ]],
+		"classname"
+	)
+end
+
 function M.find_current_method_name()
-	local parser = vim.treesitter.get_parser(0, "c_sharp")
-	local tree = parser:parse()[1]
-	local root = tree:root()
-
-	local current_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
-	current_line = current_line - 1
-
-	local query = vim.treesitter.query.parse(
-		"c_sharp",
+	return find_name_with_query(
 		[[
         (method_declaration
             name: (identifier) @methodname
         )
-    ]]
+    ]],
+		"methodname"
 	)
-
-	local start_row, _, end_row, _ = root:range()
-
-	for id, node in query:iter_captures(root, 0, start_row, end_row) do
-		local name = query.captures[id]
-		if name == "methodname" then
-			if start_row <= current_line and current_line <= end_row then
-				return vim.treesitter.get_node_text(node, 0)
-			end
-		end
-	end
-
-	return nil
 end
 
 return M
